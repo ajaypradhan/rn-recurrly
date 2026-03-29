890152f4088c3c32fcc8ed8fb8c454de2ea6ebde
Ajay Pradhan <pradhanajay14@gmail.com>
2026-03-29 14:18:19 +0530

    feat: safearea view, tab setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,17 @@
-import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex--1 bg-background p-5">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
       <Link href="/onboarding">on</Link>
       <Link href="/(auth)/sign-in">sign in</Link>
-    </View>
+    </SafeAreaView>
   );
 }
